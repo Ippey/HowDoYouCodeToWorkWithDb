f0db3266d828ad80af9a5e4141d55aa7b981602a
77web <info@77-web.com>
2020-07-08 12:47:49 +0900

    sensio/framework-extra-bundleを追加

--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,5 @@ return [
     Fidry\AliceDataFixtures\Bridge\Symfony\FidryAliceDataFixturesBundle::class => ['dev' => true, 'test' => true],
     Liip\TestFixturesBundle\LiipTestFixturesBundle::class => ['dev' => true, 'test' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
 ];
